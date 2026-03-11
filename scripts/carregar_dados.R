1cd8c4354a175cd7c9324ddb46e650b42bccc0f8
frncisc0 <156280269+frncisc0@users.noreply.github.com>
2026-03-10 23:53:37 -0300

    Adicionar script de carregamento dos dados

---
+++ scripts/carregar_dados.R
@@ -0,0 +1,25 @@
+# Carregar datasets de acidentes da PRF
+acidentes2021 <- read.csv2("data/datatran2021.csv", fileEncoding="ISO-8859-1")
+acidentes2022 <- read.csv2("data/datatran2022.csv", fileEncoding="ISO-8859-1")
+acidentes2023 <- read.csv2("data/datatran2023.csv", fileEncoding="ISO-8859-1")
+acidentes2024 <- read.csv2("data/datatran2024.csv", fileEncoding="ISO-8859-1")
+acidentes2025 <- read.csv2("data/datatran2025.csv", fileEncoding="ISO-8859-1")
+
+# Verificar dimensões dos datasets
+dim(acidentes2021)
+dim(acidentes2022)
+dim(acidentes2023)
+dim(acidentes2024)
+dim(acidentes2025)
+
+# Unir todos os anos em um único dataset
+acidentes_total <- rbind(
+  acidentes2021,
+  acidentes2022,
+  acidentes2023,
+  acidentes2024,
+  acidentes2025
+)
+
+# Verificar dimensão final
+dim(acidentes_total)
